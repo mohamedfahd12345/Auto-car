05b3cbf85768c141a820a66a4ee2371f1877322e
mohamedfahd12345 <100519172+mohamedfahd12345@users.noreply.github.com>
2022-08-14 15:09:58 +0200

    try temp things dont worry

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ let user_name=document.getElementById('username');
 let user_password=document.getElementById('password');
 
 var btn=document.getElementById('btn');
-
+var temp=document.getElementById('temp');
+temp.onclick=function(){  
+    location.href="all.html";
+}
 btn.onclick=function(){    
     if(user_name.value==="admin"&&user_password.value==="12345"){
         //location.href="https://www.google.com/";
